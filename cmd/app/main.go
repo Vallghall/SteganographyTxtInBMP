3f90502b444ba8c03f495d143ff91a4a720d0c21
Vallghall <romangusev2001@mail.ru>
2022-02-27 00:41:58 +0300

    Add final fixes

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -2,20 +2,23 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"stega/pkg/stega"
 )
 
 const (
 	originalFileName = "oberon.bmp"
-	secretText       = "Гусев Роман Михайлович"
+	secretFileName   = "secret.txt"
 	result           = "encoded.bmp"
 )
 
 func main() {
-	stega.HideInfo(originalFileName, secretText, result)
+	secretText, _ := os.ReadFile(secretFileName)
+
+	stega.HideInfo(originalFileName, string(secretText), result)
 
 	secretGot := stega.ExtractLSBInfo(len(secretText), result)
-	fmt.Println(secretGot)
+	fmt.Printf("Извлеченное сообщение: %s\n", secretGot)
 
 	mse, nmse := stega.EvalQuality(originalFileName, result)
 	fmt.Printf("СКО: %.04f\nНСКО: %f\n", mse, nmse)
